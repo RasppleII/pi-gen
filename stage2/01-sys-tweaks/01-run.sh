4764860f392764eec97502f8f001f2e55708319d
T. Joseph Carter <tjcarter@spiritsubstance.com>
2016-09-15 02:58:09 -0700

    stage2: Replace sudoers patch with sudoers.d file
    The sudoers.d/group_sudo file requires that the pi user be added to the
    sudo group, which is also done.  This allows the same sudo mod to work
    for Raspbian and Debian systems.  Fewer things that can accidentally
    break.
    
    This patch is being offered upstream because it avoids patching a
    conffile the user may someday be asked about preserving "their" changes
    to, see RPi-Distro/pi-gen#18 for details of that.

--- stage2/01-sys-tweaks/01-run.sh
+++ stage2/01-sys-tweaks/01-run.sh
@@ -8,6 +8,8 @@ install -d						${ROOTFS_DIR}/etc/systemd/system/rc-local.service.d
 install -m 644 files/ttyoutput.conf			${ROOTFS_DIR}/etc/systemd/system/rc-local.service.d/
 
 install -m 644 files/50raspi				${ROOTFS_DIR}/etc/apt/apt.conf.d/
+install -m 440 files/group_sudo				${ROOTFS_DIR}/etc/sudoers.d
+
 
 
 on_chroot sh -e - <<EOF
@@ -25,7 +27,7 @@ on_chroot sh -e - << \EOF
 for GRP in input spi i2c gpio; do
 	groupadd -f -r $GRP
 done
-for GRP in adm dialout cdrom audio users sudo video games plugdev input gpio spi i2c netdev; do
+for GRP in adm dialout cdrom audio users sudo video games plugdev input gpio spi i2c netdev sudo; do
   adduser pi $GRP
 done
 EOF
